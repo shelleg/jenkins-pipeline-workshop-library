8543456edb78463437c5d52f150820d810f7fec4
Shahar Solomianik <shahar@gnift.com>
2018-05-17 17:35:00 +0300

    t

--- vars/buildprep.groovy
+++ vars/buildprep.groovy
@@ -1,4 +1,4 @@
-def call(String buildID, String version, String project){
+def call(String buildID, String project){
   // calculate GIT lastest commit short-hash
   gitCommitHash = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
   shortCommitHash = gitCommitHash.take(7)
