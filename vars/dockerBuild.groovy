ce15499fb8e48a86f1fe0c90f1387ec8c6873ad9
hilabb <36125116+hilabb@users.noreply.github.com>
2018-05-17 17:56:04 +0300

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,5 +2,5 @@ def call() {
   // Build the docker image using a Dockerfile
   docker.build("$IMAGE")
   // override the latest tag
-  docker.build("$IMAGE_LATEST")
+  docker.build("${env.IMAGE_LATEST}")
 }
