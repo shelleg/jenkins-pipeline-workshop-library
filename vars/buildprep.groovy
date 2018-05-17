300957c546be156c1e8a4656041f7f87fafffa05
Shahar Solomianik <shahar@gnift.com>
2018-05-17 17:53:11 +0300

    image_latestdsfsd

--- vars/buildprep.groovy
+++ vars/buildprep.groovy
@@ -1,11 +1,12 @@
 def call(String buildID, String project){
-  // // calculate GIT lastest commit short-hash
-  // gitCommitHash = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-  // shortCommitHash = gitCommitHash.take(7)
-  // // calculate a sample version tag
-  // VERSION = shortCommitHash
-  // // set the build display name
-  // currentBuild.displayName = "#${buildID}-${VERSION}"
-  // IMAGE = "$project:$VERSION"
-  // IMAGE_LATEST = "$project:latest"
+  // calculate GIT lastest commit short-hash
+  gitCommitHash = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+  shortCommitHash = gitCommitHash.take(7)
+  // calculate a sample version tag
+  VERSION = shortCommitHash
+  // set the build display name
+  currentBuild.displayName = "#${buildID}-${VERSION}"
+  IMAGE = "$project:$VERSION"
+  IMAGE_LATEST = "$project:latest"
+  return IMAGE_LATEST
 }
