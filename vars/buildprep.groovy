9cf95d2116685d468d758b02f8117f3e298d1e9c
Shahar Solomianik <shahar@gnift.com>
2018-05-17 17:41:35 +0300

    t

--- vars/buildprep.groovy
+++ vars/buildprep.groovy
@@ -1,11 +1,11 @@
 def call(String buildID, String project){
-  // calculate GIT lastest commit short-hash
-  gitCommitHash = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-  shortCommitHash = gitCommitHash.take(7)
-  // calculate a sample version tag
-  VERSION = shortCommitHash
-  // set the build display name
-  currentBuild.displayName = "#${buildID}-${VERSION}"
-  IMAGE = "$project:$version"
-  IMAGE_LATEST = "$project:latest"
+  // // calculate GIT lastest commit short-hash
+  // gitCommitHash = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+  // shortCommitHash = gitCommitHash.take(7)
+  // // calculate a sample version tag
+  // VERSION = shortCommitHash
+  // // set the build display name
+  // currentBuild.displayName = "#${buildID}-${VERSION}"
+  // IMAGE = "$project:$VERSION"
+  // IMAGE_LATEST = "$project:latest"
 }
